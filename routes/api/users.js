0800d18c339141376868a40c56b96e5d8e487794
Emanuel <manny.cov09@gmail.com>
2018-12-28 15:45:01 -0800

    complete passport jwt auth on backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -93,7 +93,11 @@ router.post('/login', (req, res) => {
 // @desc Return current user
 // @access Private
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
-  res.json({msg: 'success'});
+  res.json({
+    id: req.user.id,
+    name: req.user.name,
+    email: req.user.email
+  });
 });
 
 module.exports = router;
