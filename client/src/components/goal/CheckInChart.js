2ca2062bdf141eb2dbb1d82a573ea88674ee7a80
Emanuel <manny.cov09@gmail.com>
2019-05-27 19:10:59 -0700

    more code cleanup

--- client/src/components/goal/CheckInChart.js
+++ client/src/components/goal/CheckInChart.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Bar } from 'react-chartjs-2';
-import Moment from 'react-moment';
 import moment from 'moment'
 
 class CheckInChart extends Component {
