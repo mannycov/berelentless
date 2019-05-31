4d96efa416f26ffee1952668c75e7d097fd6b344
Emanuel <manny.cov09@gmail.com>
2019-05-30 19:03:40 -0700

    code cleanup edit footer edit landing page

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className="bg-dark text-white mt-5 p-4 text-center" style={{position: 'bottom', width: '100%'}}>
+    <footer className="bg-dark text-white mt-5 p-4 text-center" style={{position: 'absolute', bottom: '0', width: '100%', height: '70px'}}>
       Copyright &copy; {new Date().getFullYear()} BeRelentless
     </footer>
   )
 }
-
-// bg-dark text-white mt-5 p-4 text-center
-// style={{position: 'bottom', width: '100%'}}
