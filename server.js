0deeb3b4661f1098fa87bf84e847cf25a022051c
Emanuel <manny.cov09@gmail.com>
2019-06-17 17:43:56 -0700

    edit

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.use('/api/profile', profile);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set a static foler
-  // app.use('/static', express.static(path.join(__dirname, 'client/build')));
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
