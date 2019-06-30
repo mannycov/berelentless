4ae76b18b22ddd4a1c73ea88e31fb7db380c0a43
Emanuel <manny.cov09@gmail.com>
2019-06-30 10:27:27 -0700

    edit store config

--- client/src/store.js
+++ client/src/store.js
@@ -1,5 +1,4 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -9,21 +8,11 @@ const middleware = [thunk];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-// const reduxDevTools = (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) || compose;
-
 const store = createStore(
   rootReducer,
   initialState,
   composeEnhancers(
     applyMiddleware(...middleware)
 ));
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   composeWithDevTools(
-//     applyMiddleware(...middleware),
-//     reduxDevTools
-// ));
 
 export default store;
